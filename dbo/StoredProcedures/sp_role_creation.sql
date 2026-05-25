11b1d2397b117c5b01bc85b699a1fb00367c0a64
Kristen Barringer <kristen.barringer@contractor.tranetechnologies.com>
2026-05-25 10:47:01 -0500

    comment out proc

--- dbo/StoredProcedures/sp_role_creation.sql
+++ dbo/StoredProcedures/sp_role_creation.sql
@@ -3,16 +3,18 @@ CREATE   procedure [dbo].[sp_role_creation]
 
 AS
 BEGIN
+print 'TODO FIX THIS'
+/* TODO FIX THIS and add role_type to lookup_code
 DECLARE @role_rid INT;
-    INSERT INTO role (description, standard_role, active, user_type_rid, tenant_id, name)
+    INSERT INTO role (description, is_standard_role, active, user_type_rid, tenant_id, name)
     VALUES ('ADMIN', 1, 1, 1, @tenant_id, 'ADMIN');
 	SET @role_rid = SCOPE_IDENTITY();
 	EXEC sp_role_permission_mapping_admin @role_rid
 
-	INSERT INTO role (description, standard_role, active, user_type_rid, tenant_id, name)
+	INSERT INTO role (description, is_standard_role, active, user_type_rid, tenant_id, name)
     VALUES ('BASIC', 1, 1, 1, @tenant_id, 'BASIC');
 	SET @role_rid = SCOPE_IDENTITY();
 	EXEC sp_role_permission_mapping_basic @role_rid
-END;
+*/END;
 GO
 
