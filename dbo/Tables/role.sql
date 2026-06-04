435d659634ec5d101d597d3b071bca0f8f46fbff
Kristen Barringer <kristen.barringer@contractor.tranetechnologies.com>
2026-06-04 14:36:25 -0500

    move tenant, role, user to Account db

---
+++ dbo/Tables/role.sql
@@ -0,0 +1,65 @@
+CREATE TABLE [dbo].[role]
+(
+    -- ------------------------------------
+    -- pks and main uq columns
+    [role_uuid] UNIQUEIDENTIFIER CONSTRAINT [df_role_id] DEFAULT (NEWID()) NOT NULL,
+    [role_rid] INT IDENTITY (1, 1) NOT NULL,
+    -- fk columns - to tenant
+    [tenant_uuid] UNIQUEIDENTIFIER NOT NULL,
+    -- every table must have a tenant_uuid  
+    -- main attribute columns of this entity 
+    [name] NVARCHAR (100) NOT NULL,
+    [description] NVARCHAR (100) NOT NULL,
+    -- fk columns - other main fks
+    -- fk columns - to lookup code
+    [role_type_code] VARCHAR(30) NOT NULL,
+    -- bit flag columns
+    [is_active] BIT CONSTRAINT [df_role_is_active] DEFAULT (1) NOT NULL,
+    [is_standard_role] BIT CONSTRAINT [df_role_is_standard_role] DEFAULT (1) NOT NULL,
+    -- date columns
+    [created_date] DATETIME CONSTRAINT [df_role_created_date] DEFAULT (getdate()) NOT NULL,
+    [updated_date] DATETIME CONSTRAINT [df_role_updated_date] DEFAULT (getdate()) NOT NULL,
+    -- fk columns - to user
+    [created_by_user_uuid] UNIQUEIDENTIFIER NULL,
+    -- note columns
+    [notes] NVARCHAR (1000) NULL,
+    -- test data columns (only used for test data process on dev)
+    [test_data_group] int null  ,
+    -- columns to be deprecated
+    [created] DATETIME CONSTRAINT [df_role_created] DEFAULT (getdate()) NOT NULL,
+    [active] BIT NOT NULL,
+);
+GO
+
+-- ------------------------------------
+-- pks and main uq indexes
+ALTER TABLE [dbo].[role]
+    ADD CONSTRAINT [cix_role_rid] PRIMARY KEY CLUSTERED ([role_rid] ASC) WITH (FILLFACTOR = 100, DATA_COMPRESSION = PAGE);
+GO
+ALTER TABLE dbo.[role]
+ADD CONSTRAINT uk_role_uuid UNIQUE ([role_uuid]);
+GO
+-- fks - to tenant
+ALTER TABLE [dbo].[role] ADD CONSTRAINT [fk_role_tenant_uuid] FOREIGN KEY ([tenant_uuid]) REFERENCES [dbo].[tenantinfo] ([tenant_uuid]); 
+ GO
+CREATE NONCLUSTERED INDEX [ix_fk_role_tenant_uuid] -- add explicit index for the FK column, to improve join performance
+  ON [dbo].[role]([tenant_uuid] ASC); 
+  GO
+-- -- fks - to user - TODO circular logic - need to rethink this
+-- ALTER TABLE [dbo].[role]
+--     ADD CONSTRAINT [fk_role_user_uuid] FOREIGN KEY ([created_by_user_uuid]) REFERENCES [dbo].[user_accounts] ([user_uuid]);
+-- GO
+-- CREATE NONCLUSTERED INDEX [ix_fk_role_user_uuid] -- add explicit index for the FK column, to improve join performance
+--     ON [dbo].[role]([created_by_user_uuid] ASC);
+-- GO
+-- fks - other main fks
+-- fks - to lookup code
+ALTER TABLE [dbo].[role]
+    ADD CONSTRAINT [fk_role_role_type_code] FOREIGN KEY ([role_type_code]) REFERENCES [dbo].[lookup_code] ([code]);
+GO
+CREATE NONCLUSTERED INDEX [ix_fk_role_role_type_code] -- add explicit index for the FK column, to improve join performance
+    ON [dbo].[role]([role_type_code] ASC);
+GO
+-- other constraints and indexes 
+-- N/A
+-- END
---
+++ dbo/Tables/tenantinfo.sql
@@ -0,0 +1,70 @@
+CREATE TABLE [dbo].[tenantinfo]
+(
+    -- ------------------------------------
+    -- pks and main uq columns
+    [tenant_uuid] UNIQUEIDENTIFIER CONSTRAINT [df_tenantinfo_tenant_uuid] DEFAULT (NEWID()) NOT NULL,
+    -- fk columns - to tenant
+    -- N/A
+    -- main attribute columns of this entity 
+    [organization] varchar(255),
+    [company_name] varchar(255),
+    [minimum_user] INT NULL,
+    [customer_rid] INT NULL,
+    -- fk columns - other main fks
+    -- N/A
+    -- fk columns - to lookup code    
+    [work_schedule_code] VARCHAR (30) CONSTRAINT [df_tenantinfo_work_schedule_code] DEFAULT ('WRK_TODO') NOT NULL, -- todo came from work_schedule
+    -- N/A
+    -- bit flag columns
+    [is_active] BIT CONSTRAINT [df_tenantinfo_is_active] DEFAULT (1) NOT NULL,
+    -- date columns
+    [created_date] DATETIME CONSTRAINT [df_tenantinfo_created_date] DEFAULT (getdate()) NOT NULL,
+    [updated_date] DATETIME CONSTRAINT [df_tenantinfo_updated_date] DEFAULT (getdate()) NOT NULL,
+    -- fk columns - to user
+    [created_by_user_uuid] UNIQUEIDENTIFIER NULL,
+    -- note columns
+    [notes] NVARCHAR (1000) NULL,
+    -- N/A
+    -- test data columns (only used for test data process on dev)
+    [test_data_group] int null,
+    -- columns to be deprecated
+    [Pipeline_Id] NVARCHAR (100) DEFAULT (NULL) NULL,
+    [created] DATETIME CONSTRAINT [df_tenantinfo_created] DEFAULT (getdate()) NOT NULL,
+    [v2_user_rid] INT NULL,
+    [user_rid] INT NULL,
+    [user_uuid] UNIQUEIDENTIFIER NULL,
+    [user_name] varchar(255),
+
+);
+GO
+-- ------------------------------------
+-- pks and main uq indexes
+ALTER TABLE [dbo].[tenantinfo]
+    ADD CONSTRAINT [cix_tenantinfo_tenant_uuid] PRIMARY KEY CLUSTERED ([tenant_uuid] ASC) WITH (FILLFACTOR = 100, DATA_COMPRESSION = PAGE);
+GO
+-- fks - to tenant
+-- N/A
+-- fks - to user -- TODO this creates a circular dependency so let's rethink this
+-- ALTER TABLE [dbo].[tenantinfo]
+--     ADD CONSTRAINT [fk_tenantinfo_user_uuid] FOREIGN KEY ([user_uuid]) REFERENCES [dbo].[user_accounts] ([user_uuid]);
+-- GO
+-- CREATE NONCLUSTERED INDEX [ix_fk_tenantinfo_user_uuid] -- add explicit index for the FK column, to improve join performance
+--     ON [dbo].[tenantinfo]([user_uuid] ASC);
+-- GO
+-- ALTER TABLE [dbo].[tenantinfo]
+--     ADD CONSTRAINT [fk_tenantinfo_created_by_user_uuid] FOREIGN KEY ([created_by_user_uuid]) REFERENCES [dbo].[user_accounts] ([user_uuid]);
+-- GO
+-- CREATE NONCLUSTERED INDEX [ix_fk_tenantinfo_created_by_user_uuid]
+--     ON [dbo].[tenantinfo]([created_by_user_uuid] ASC);
+-- GO
+-- fks - other main fks
+-- N/A
+-- fks - to lookup code
+ALTER TABLE [dbo].[tenantinfo] ADD CONSTRAINT [fk_tenantinfo_work_schedule_code] FOREIGN KEY ([work_schedule_code]) REFERENCES [dbo].[lookup_code] ([code]); 
+GO
+CREATE NONCLUSTERED INDEX [ix_fk_tenantinfo_work_schedule_code] -- add explicit index for the FK column, to improve join performance
+ON [dbo].[tenantinfo]([work_schedule_code] ASC); 
+GO
+-- other constraints and indexes 
+-- N/A
+-- END
