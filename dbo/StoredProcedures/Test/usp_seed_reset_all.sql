e837e3df94294bedf28811cf3cf9cf45504d7b68
Kristen Barringer <kristen.barringer@contractor.tranetechnologies.com>
2026-06-18 14:15:09 -0500

    alter reset proc

--- dbo/StoredProcedures/Test/usp_seed_reset_all.sql
+++ dbo/StoredProcedures/Test/usp_seed_reset_all.sql
@@ -35,7 +35,7 @@ BEGIN
     PRINT CONCAT('  Cleared dbo.role (', @@ROWCOUNT, ' row(s)).');
     DELETE FROM dbo.db_m_tenantinfo where coalesce(notes, '') = 'TEST DATA PROCESS ON DEV';
     PRINT CONCAT('  Cleared dbo.tenantinfo (', @@ROWCOUNT, ' row(s)).'); 
-    DELETE FROM dbo.account_details --where coalesce(notes, '') = 'TEST DATA PROCESS ON DEV'; -- todo fix this
+    DELETE FROM dbo.account_details where coalesce(notes, '') = 'TEST DATA PROCESS ON DEV'; -- todo fix this
     PRINT CONCAT('  Cleared dbo.account_details (', @@ROWCOUNT, ' row(s)).');
 
     PRINT '=== Seed data reset complete ===';
